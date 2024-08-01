029f2e1311e8a694f4952ed1a8141119a52b5d17
shubhampanchal76 <shubhampanchal.p41@gmail.com>
2024-08-01 18:50:34 +0530

    files updated

--- src/de/utils_by_occ.py
+++ src/de/utils_by_occ.py
@@ -5,12 +5,11 @@ import pandas as pd
 import numpy as np
 from pyspark.sql.functions import trim, regexp_replace, col,desc
 from pyspark.sql.functions import sum as pyspark_sum
-from pyspark.sql.types import FloatType
 import pyspark.sql.functions as F
 from pyspark.sql.functions import count, mean, stddev, min, max, median
 from pyspark.sql.types import FloatType, DoubleType, IntegerType
-import pyspark.sql.functions as Fom pyspark.sql.functions import col,aggregate
-
+from pyspark.sql.functions import col,aggregate
+from pyspark.sql.functions import col, count, when
 
 
 
@@ -18,14 +17,11 @@ import pyspark.sql.functions as Fom pyspark.sql.functions import col,aggregate
 
 
 def male_ratio_per_occupation(df):
-  
- 
-  from pyspark.sql.functions import col, count, when
   male_ratio = df.groupBy('occupation') \
-                      .agg(count('*').alias('total'),
-                           count(when(col('gender') == 'M', True)).alias('male_count')) \
-                      .withColumn('male_ratio', col('male_count') / col('total')) \
-                      .orderBy(col('male_ratio').desc())
+               .agg(F.count('*').alias('total'),
+                    F.count(F.when(F.col('gender') == 'M', True)).alias('male_count')) \
+               .withColumn('male_ratio', F.col('male_count') / F.col('total')) \
+               .orderBy(F.col('male_ratio').desc())
   return male_ratio
 
 
@@ -75,23 +71,16 @@ def mean_pretestscore_nighthawks(df,colm):
   return df.filter(df.regiment == colm).selectExpr("avg(preTestScore)").collect()[0][0]
 
 
-
-
-
 def mean_pretestscore_company(df,grp_colm):
-   return df.groupBy(grp_colm).agg(F.mean("preTestScore"))
-   
+   return  df.groupBy(grp_colm).agg(F.mean("preTestScore"))
 
 
 def mean_pretestscore_regiment_company(df):
   return df.groupBy("regiment", "company").agg(F.mean("preTestScore"))
-  
 
 def regiment_company_count(df):
   return df.groupBy("regiment", "company").count()
 
 
-
 def group_by_regiment_company(df):
  return df.groupBy("regiment", "company").mean()
- 
